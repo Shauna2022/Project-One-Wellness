f6938367c70d11b2f60383cd8cfcd4deaf93362f
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-05-11 17:03:45 -0400

    followed lesson mark down to get data to work

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-// const URL = "https://lightning-yoga-api.herokuapp.com/yoga_poses"
+// const apiYoga = "https://lightning-yoga-api.herokuapp.com/yoga_poses"
 
 // i should click a button and get a random yoga pose
 
+
+
 const $yogaButton = $('#yogaButton')
-console.log($yogaButton)
+// console.log($yogaButton)
 
 const getYogaPose = () => {
-    console.log('yoga pose ready')
+$.ajax({
+    url:'https://lightning-yoga-api.herokuapp.com/yoga_poses'
+}).then(
+    (data) => {
+        console.log(data);
+    },
+    (error) => {
+        console.log('no data', error);
+    }
+);
+
 }
 
 $yogaButton.on('click' , getYogaPose)
