53e408aa515da83a6908601b312e317c43062890
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-05-11 14:17:48 -0400

    followinglesson by Ayla Ex changed button id

---
+++ app.js
@@ -0,0 +1,6 @@
+// const URL = "https://lightning-yoga-api.herokuapp.com/yoga_poses"
+
+// i should click a button and get a random yoga pose
+
+const $yogaButton = $('#button')
+console.log($getButton)
