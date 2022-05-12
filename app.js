79789a31175542401478a3395f81bed2f8860707
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-05-11 20:54:08 -0400

    added elements in HTML, styled, and worked on API

--- app.js
+++ app.js
@@ -2,23 +2,30 @@ const $yogaButton = $('#yogaButton')
 // console.log($yogaButton)
 function handleGetData(evt) {
     evt.preventDefault();
-const getYogaPose = () => {
-$.ajax({
-    URL:'https://lightning-yoga-api.herokuapp.com/yoga_poses'
-}).then(
-    (data) => {
-        console.log(data);
-    },
-    (error) => {
-        console.log('no data', error);
-    }
-);
-}
+        $.ajax({
+            URL: 'https://lightning-yoga-api.herokuapp.com/yoga_poses'
+        }).then(
+            (data) => {
+                data.items.img_url;
+                console.log(data);
+            },
+            (error) => {
+                console.log('no data', error);
+            }
+        );
 }
 
 $yogaButton.on('click' , function() {
     let $poseName = $('#poseName').val();
-    $('#poseName').append(`<li>${URL}</li>`)
+    $('#poseName').append(`<li>${data.items.img_url}</li>`)
     $('ul').val('');
 });
 
+// handleGetData(evt)
+// Charles Barlett
+// const url = 'https://lightning-yoga-api.herokuapp.com/yoga_poses'
+// $.ajax(url).then(
+//     function(data){
+//     console.log(data.items[1].img_url)
+//     }
+// )
