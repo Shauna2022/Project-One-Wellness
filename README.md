13a44214313462bb5e16251577a6a78800548bac
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-05-11 23:30:02 -0400

    cleaned up JS + started README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is a wellness resource to make sure that we are taking care of our mental health while we are taking this intense course.
+Charles Barlett, Iyana Marquez, David Aydin, Ayla Ex, Abidur
+
+ const url = 'https://lightning-yoga-api.herokuapp.com/yoga_poses'
+ $.ajax(url).then(
+         function(data){
+      console.log(data.items.img_url)
+   }
--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 const $yogaButton = $('#yogaButton')
 const $picture = $('#picture')
-// console.log($yogaButton)
 $yogaButton.on('click' , function() {
-    // alert('jQuery works')
     let num = Math.floor(Math.random() * 49)
         $.ajax({
             url: 'https://lightning-yoga-api.herokuapp.com/yoga_poses'
         }).then(
             (data) => {
-                //    $picture.img_url(data.items.img_url);
                 let img = data.items[num].img_url
                     $picture.attr("src", img)
                 console.log(img)
                 $('#name').text(data.items[num].english_name)
-                    // let $poseName = $('#name').val();
-                    // $('p').val('');
             });
             (error) => {
                 console.log('no data', error);
             }
 });
-//  handleGetData(evt)
-
-// Charles Barlett, Iyana Marquez, David Aydin
-// const url = 'https://lightning-yoga-api.herokuapp.com/yoga_poses'
-// $.ajax(url).then(
-//     function(data){
-//     console.log(data.items.img_url)
-//     }
-// )
